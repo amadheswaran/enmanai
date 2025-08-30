c6da5f76cd38e3c9c161e09260beaa127b21fa51
Arunkumar Madheswaran <43840802+amadheswaran@users.noreply.github.com>
2025-08-30 15:23:26 +0530

    Initial commit with base files

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ["maps.googleapis.com", "images.unsplash.com"]
+  }
+}
+module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        tamil: ['Noto Sans Tamil', 'Latha', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
